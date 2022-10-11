5e0f0a8d367d1df3208e8c78deaea6bd0f230a2b
Asel Esra Ozyilmaz <esra.ozyilmaz@iaau.edu.kg>
2022-10-11 20:49:28 +0600

    created the project

---
+++ src/main/java/com/example/light_up_travel/LightUpTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.light_up_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightUpTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightUpTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/light_up_travel/LightUpTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.light_up_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightUpTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
