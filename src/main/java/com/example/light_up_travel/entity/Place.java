801a8f6c7b47cb281ef2b3d56370becf3f53b75c
Zhanyla22 <ja.mamytova@gmail.com>
2022-12-09 19:15:45 +0600

    fixed bugs

--- src/main/java/com/example/light_up_travel/entity/Place.java
+++ src/main/java/com/example/light_up_travel/entity/Place.java
@@ -1,12 +1,23 @@
 package com.example.light_up_travel.entity;
 
 import com.example.light_up_travel.enums.Status;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Entity(name = "places")
 public class Place {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
