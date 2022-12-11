0ba83ba95fe16b5cd5fd4f0d05092ad6927a9f76
Zhanyla22 <ja.mamytova@gmail.com>
2022-12-11 18:35:10 +0600

    Lifehack Delete by Id changed

--- src/main/java/com/example/light_up_travel/controller/PostController.java
+++ src/main/java/com/example/light_up_travel/controller/PostController.java
@@ -63,8 +63,8 @@ public class PostController {
 
     @Operation(summary = "delete post by id - only for client side")
     @DeleteMapping("/delete-post/{id}")
-    public ResponseEntity<Void> deletePostById(@RequestBody GetPostDTO getPostDTO){
-        postService.deletePostById(getPostDTO.getId());
+    public ResponseEntity<Void> deletePostById(@PathVariable Long id){
+        postService.deletePostById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
     @Operation(summary = "like Post")
